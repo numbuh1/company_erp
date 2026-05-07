905df6eeaccbbc144c9b863f62e521fe1b017c0c
Huy Nguyen <huy.nguyen@ascension6.com.au>
2026-05-07 11:53:49 +0700

    Fix logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <img
-    src="https://media.licdn.com/dms/image/v2/D560BAQHjWTjfkwzbew/company-logo_100_100/B56ZakwSgDHUAQ-/0/1746520844862/ascension_6_logo?e=1778112000&v=beta&t=RIEVOwyx78Op6_JRC_CL5JdcT2NLdq2yxVmzUQxxqho"
+    src="{{ asset('images/logo.png') }}"
     alt="Ascension 6"
     {{ $attributes }}
 >
